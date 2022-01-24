b79a9bbfa73b7a4671987252acfdc03776823e64
Alonesa <alenakrasnov@mail.ru>
2022-01-24 19:20:44 +0200

    added simple menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import {Header} from './components/Header';
+import {AppBarMenu} from './components/AppBarMenu';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <AppBarMenu/>
     </div>
   );
 }
