7cd9955997d78b300c24bb3ff0c520af487b0fd1
Alonesa <alenakrasnov@mail.ru>
2022-01-24 21:49:30 +0200

    added main content basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import {AppBarMenu} from './components/AppBarMenu';
+import {MainContent} from './components/MainContent';
 
 function App() {
   return (
     <div className="App">
       <AppBarMenu/>
+      <MainContent/>
     </div>
   );
 }
---
+++ src/components/MainContent.tsx
@@ -0,0 +1,15 @@
+import {Box, Container, Grid} from '@material-ui/core';
+import React from 'react';
+
+export const MainContent = () => {
+  return (
+	<Container>
+	  <Grid container alignContent={'center'} justifyContent={'space-between'}>
+		<Grid item xs={12} sm={3}>sidebar
+		  search</Grid>
+		<Grid item xs={12} sm={9}>Main
+		  content</Grid>
+	  </Grid>
+	</Container>
+  );
+};
