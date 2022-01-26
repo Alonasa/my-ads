f1392664019da2e62f8ebf1bca10abf2b720b40c
Alonesa <alenakrasnov@mail.ru>
2022-01-27 00:10:19 +0200

    finish footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import {Container} from '@material-ui/core';
+import {Container, Grid} from '@material-ui/core';
+import s from './Footer.module.css'
+
+const pages = ['Page 1', 'Page 2']
 
 export const Footer = () => {
   return (
-	<footer className='footer' style={{background: '#121e62', minHeight: '50px', color: '#ffffff', display: 'flex', marginTop: 'auto'}}>
-		<Container>
-		Hi
-		</Container>
+	<footer className={s.Footer}>
+	  <Container>
+		<Grid container justifyContent={'space-between'} alignItems={'baseline'} spacing={2}>
+		  <Grid item xs={12} sm={2}>
+			<h3>My-Ads</h3>
+		  </Grid>
+		  <Grid item xs={12} sm={10}>
+			<ul className={s.Footer__menu}>
+			  {pages.map(p => {
+				return (
+				  <li key={p}><a href='#'>{p}</a></li>
+				)
+			  })}
+			</ul>
+		  </Grid>
+		</Grid>
+	  </Container>
 	</footer>
   );
 };
