bc8988c8d75836914b8d7e77529e8b61b1f6bb61
Alonesa <alenakrasnov@mail.ru>
2022-01-26 22:56:11 +0200

    added icon props to button component

--- src/components/UniversalButton.tsx
+++ src/components/UniversalButton.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import {Button} from '@material-ui/core';
+import React, {ReactElement} from 'react';
+import {Button, makeStyles, SvgIconProps} from '@material-ui/core';
 
 type PropsType = {
   title: string
   onClick: () => void
   variant?: 'contained' | 'outlined' | 'text'
   color?: string
+  endIcon?: ReactElement<SvgIconProps>
 }
 
 export const UniversalButton = (props: PropsType) => {
   return (
 	<div>
-	  <Button onClick={props.onClick} variant={props.variant} color={'primary'}>{props.title}</Button>
+	  <Button onClick={props.onClick} variant={props.variant} color={'primary'}
+			  endIcon={props.endIcon}>{props.title}</Button>
 	</div>
   );
 };
