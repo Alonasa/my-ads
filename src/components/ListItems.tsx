b57f69793302bbb33ea807c5af7ea877426eec56
Alonesa <alenakrasnov@mail.ru>
2022-01-27 14:33:41 +0200

    addeв container for list items and fix gutters for a small resolution

--- src/components/ListItems.tsx
+++ src/components/ListItems.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {ProductItemShort} from './ProductItemShort';
-
+import s from './ListItems.module.css'
+import {Container} from '@material-ui/core';
 
 export const ListItems = () => {
   const items = [
@@ -25,12 +26,36 @@ export const ListItems = () => {
 	  currency: '$',
 	  isFavorite: true,
 	  isTop: true
+	},
+	{
+	  id: 2,
+	  title: 'I am item',
+	  description: 'Please add description',
+	  price: 1001,
+	  location: 'Kiev',
+	  date: 'today',
+	  currency: '$',
+	  isFavorite: true,
+	  isTop: true
+	},
+	{
+	  id: 3,
+	  title: 'I am item',
+	  description: 'Please add description',
+	  price: 1001,
+	  location: 'Kiev',
+	  date: 'today',
+	  currency: '$',
+	  isFavorite: true,
+	  isTop: true
 	}
   ]
   
   return (
-	<ul style={{padding: 0, display: 'flex', justifyContent: 'space-between'}}>
-	  <ProductItemShort items={items}/>
-	</ul>
+	 <Container className={s.List__container}>
+	   <ul className={s.List}>
+		 <ProductItemShort items={items}/>
+	   </ul>
+	 </Container>
   );
 };
