ac88ec3578f8c3901cafc07beea5878be2834ccf
Alonesa <alenakrasnov@mail.ru>
2022-01-30 01:32:21 +0200

    fix profile page

--- src/components/Pages/Profile/ProfileContent.tsx
+++ src/components/Pages/Profile/ProfileContent.tsx
@@ -1,31 +1,35 @@
-import {Avatar, Container} from '@material-ui/core';
+import {Box, Container} from '@material-ui/core';
 import React from 'react';
 import s from './Profile.module.css';
-import {Message} from '../Dialogs/Message';
 
 
 export const ProfileContent = () => {
   return (
-	<Container>
-	  <div>
-		<img className={s.Profile__content_img} alt={'avatar'} width={250} height={250} src="https://i.stack.imgur.com/l60Hf.png"/>
-	  </div>
-	  <ul>
-		<li className={s.Profile__content_item}><span
-		  className={s.Profile__content_title}>Days on website</span><span>10</span>
-		</li>
-		<li className={s.Profile__content_item}><span className={s.Profile__content_title}>Info</span><span></span>Hi
-		  for everyone. I am James Bond like to sell my useless clothes
-		</li>
-		<li className={s.Profile__content_item}><span
-		  className={s.Profile__content_title}>Total Ads</span><span>15</span>
-		</li>
-		<li className={s.Profile__content_item}><span className={s.Profile__content_title}>Location</span><span>London, UK</span>
-		</li>
-	  </ul>
-	  
-	  <Message/>
-	</Container>
+	<Box m={{xs:0, sm: 3}}>
+	  <Container>
+		<div className={s.Profile__img}>
+		  <img className={s.Profile__content_img} alt={'avatar'} width={250}
+			   height={250} src="https://i.stack.imgur.com/l60Hf.png"/>
+		</div>
+		<ul style={{padding: 0}}>
+		  <li className={s.Profile__content_item}><span
+			className={s.Profile__content_title}>Days on website</span><span>10</span>
+		  </li>
+		  <li className={s.Profile__content_item}><span
+			className={s.Profile__content_title}>Info</span>
+			<span>Hi for everyone. I am James Bond like to sell my useless clothes</span>
+		  </li>
+		  <li className={s.Profile__content_item}><span
+			className={s.Profile__content_title}>Total Ads</span>
+			<span>15</span>
+		  </li>
+		  <li className={s.Profile__content_item}><span
+			className={s.Profile__content_title}>Location</span>
+			<span>London, UK</span>
+		  </li>
+		</ul>
+	  </Container>
+	</Box>
   );
   
 };
