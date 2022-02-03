44279253b6f217704be915bb451184b6b369c648
Alonesa <alenakrasnov@mail.ru>
2022-02-03 23:02:40 +0200

    added basic dialog data

--- src/components/Pages/Dialogs/Dialogs.tsx
+++ src/components/Pages/Dialogs/Dialogs.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import {Message} from '../../Messages/Message';
+import {AuthMenu} from '../../AuthMenu/AuthMenu';
+import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
   return (
-	<div>
+	<div className={s.dialogs}>
+	  <AuthMenu/>
+	  <ul className={s.dialogs__list}>
+		<li className={s.dialog__item}><span>Inna</span></li>
+		<li className={s.dialog__item}><span>Daniel</span></li>
+		<li className={s.dialog__item}><span>Adolf</span></li>
+		<li className={s.dialog__item}><span>Janna</span></li>
+	  </ul>
 	 <Message/>
 	</div>
   );
