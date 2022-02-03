d0cf38c7f7ada11567af2eef61f6d3f714383698
Alonesa <alenakrasnov@mail.ru>
2022-02-03 23:26:38 +0200

    added simple dialog styles

--- src/components/Pages/Dialogs/Dialogs.tsx
+++ src/components/Pages/Dialogs/Dialogs.tsx
@@ -2,18 +2,25 @@ import React from 'react';
 import {Message} from '../../Messages/Message';
 import {AuthMenu} from '../../AuthMenu/AuthMenu';
 import s from './Dialogs.module.css'
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import {Container} from '@material-ui/core';
 
 export const Dialogs = () => {
   return (
-	<div className={s.dialogs}>
+	<div>
 	  <AuthMenu/>
-	  <ul className={s.dialogs__list}>
-		<li className={s.dialog__item}><span>Inna</span></li>
-		<li className={s.dialog__item}><span>Daniel</span></li>
-		<li className={s.dialog__item}><span>Adolf</span></li>
-		<li className={s.dialog__item}><span>Janna</span></li>
-	  </ul>
-	 <Message/>
+	  <Container className={s.dialogs}>
+		<Grid container style={{margin: '25px 0',}}>
+		  <ul className={s.dialogs__list}>
+			<li className={s.dialog__item}><span>Inna</span></li>
+			<li className={s.dialog__item}><span>Daniel</span></li>
+			<li className={s.dialog__item}><span>Adolf</span></li>
+			<li className={s.dialog__item}><span>Janna</span></li>
+		  </ul>
+		  <Message/>
+		</Grid>
+	  </Container>
 	</div>
   );
 };
